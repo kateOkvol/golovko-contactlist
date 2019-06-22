202b8ec36e18bd64d52f96179a99ff3174f678d5
kateOkvol <kate.okvol@gmail.com>
2019-06-22 13:02:04 +0300

    update js

--- module-web/web/js/onloadScripts.js
+++ module-web/web/js/onloadScripts.js
@@ -7,34 +7,27 @@ function showContacts() {
 }
 
 function createTable() {
-   // if (request.readyState === 4 && request.status === 200) {
-   //     var contactsList = JSON.parse(request.response);
+    if (request.readyState === 4 && request.status === 200) {
+        var contactsList = JSON.parse(request.response);
 
-        // var tableHTML = "<style type=\"text/css\"> TABLE {" +
-        //     " border-collapse: collapse; " +
-        //     "border: 1px solid black;} " +
-        //     "TD, TH {border: 1px solid black;}" +
-        //     "</style>";
-
-       var tableHTML = "";
-        tableHTML += "<table id=\"contact-table\">";
+        var tableHTML = "<table id=\"contact-table\">";
         tableHTML += "<tr>";
         tableHTML += "<td>Full name</td> <td>Birth date</td> <td>Address</td> <td>Company</td>";
         tableHTML += "</tr>";
         tableHTML += "</table>";
-        //
-        // for (var i = 0; i < contactsList.length; i++) {
-        //     tableHTML += "<tr>";
-        //     tableHTML += "<td>" + contactsList[i].fullName + "</td> " +
-        //         "<td>" + contactsList[i].birthDate + "</td> " +
-        //         "<td>" + contactsList[i].address + "</td> " +
-        //         "<td>" + contactsList[i].company + "</td>";
-        //     tableHTML += "</tr>";
-        // }
+
+        for (var i = 0; i < contactsList.length; i++) {
+            tableHTML += "<tr>";
+            tableHTML += "<td>" + contactsList[i].fullName + "</td> " +
+                "<td>" + contactsList[i].birthDate + "</td> " +
+                "<td>" + contactsList[i].address + "</td> " +
+                "<td>" + contactsList[i].company + "</td>";
+            tableHTML += "</tr>";
+        }
         document.getElementById("contact-table").innerHTML = tableHTML;
-    // } else {
-    //     console.log("cant create table");
-    // }
+    } else {
+        console.log("cant create table");
+    }
 }
 
 
